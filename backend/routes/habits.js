db24da12c250e65c8ce391432982b98ce2bbfcd5
lohs <55119191+lohs@users.noreply.github.com>
2021-02-26 00:06:29 -0500

    added check for duplicate habit insertion

--- backend/routes/habits.js
+++ backend/routes/habits.js
@@ -1,10 +1,9 @@
 const router = require("express").Router(); //needed because we're creating a route
 const Habit = require("../models/habit.model");
-let User = require("../models/habit.model"); //mongoose model
 
 //first route 'localhost:5000/habits/'
 router.route("/").get((req, res) => {
-  User.find() //mongoose method that gets all habits from MongoDB Atlas DB; returns a promise
+  Habit.find() //mongoose method that gets all habits from MongoDB Atlas DB; returns a promise
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
@@ -16,41 +15,53 @@ router.route("/add").post((req, res) => {
   const dailyCompleted = req.body.dailyCompleted;
   //const TotalCount = req.Body.TotalCount;
 
-  const newHabit = new Habit({ username, description, dailyCompleted });
-
-  newHabit.save()
-    .then(() => res.json("Habit added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+  // check if duplicate habit
+  Habit.findOne({ username: username, description: description }).then(
+    (habit) => {
+      if (habit) {
+        return res.status(400).json("Error: User already has this habit!");
+      } else {
+        const newHabit = new Habit({ username, description, dailyCompleted });
+        newHabit
+          .save()
+          .then(() => res.json("Habit added!"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      }
+    }
+  );
 });
 
 // '/:id' - MongoDB ID
-router.route('/:id').get((req, res) => {
-    Habit.findById(req.params.id)
-    .then(habit => res.json(habit))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/:id").get((req, res) => {
+  Habit.findById(req.params.id)
+    .then((habit) => res.json(habit))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // delete habit
-router.route('/:id').delete((req, res) => {
-    Habit.findByIdAndDelete(req.params.id)
-    .then(habit => res.json('Habit deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/:id").delete((req, res) => {
+  Habit.findByIdAndDelete(req.params.id)
+    .then((habit) =>
+      res.json(`${habit.username}: ${habit.description} deleted!`)
+    )
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 //update habit
-router.route('/update/:id').post((req, res) => {
-    Habit.findById(req.params.id)
-    .then(habit => {
-        habit.username = req.body.username;
-        habit.Description = req.body.Description;
-        // habit.DailyCompleted = req.body.DailyCompleted;
-        // habit.TotalCount = req.body.TotalCount;
+router.route("/update/:id").post((req, res) => {
+  Habit.findById(req.params.id)
+    .then((habit) => {
+      habit.username = req.body.username;
+      habit.Description = req.body.Description;
+      // habit.DailyCompleted = req.body.DailyCompleted;
+      // habit.TotalCount = req.body.TotalCount;
 
-        habit.save()
-        .then(()=> res.json('Habit updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+      habit
+        .save()
+        .then(() => res.json("Habit updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
     })
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
