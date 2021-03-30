88ba50288fe0b2ed780cbb0fb1c9b75a7663130d
lohs <55119191+lohs@users.noreply.github.com>
2021-03-30 16:05:56 -0400

    implemented profile card for dashboard

--- habit-tracker/src/Pages/Dashboard.js
+++ habit-tracker/src/Pages/Dashboard.js
@@ -1,32 +1,74 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
-import { Button, Input } from '@material-ui/core';
-import { auth } from "../firebase";
-import { Route, Redirect } from 'react-router-dom';
-import { AuthContext } from "../context";
+// import { Button, Input } from "@material-ui/core";
+// import { auth } from "../firebase";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.js";
+import { Card, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
-const Dashboard = () => {
-    const { user, setUser } = useContext(AuthContext);
-    return(
-        <div>
-            {
-            user ? 
-            (
-                <div>
-                    <Navbar/>
-                    <p>{`Welcome, ${user.displayName}`}</p>
-                    <h1>Habits Stuff</h1>
-                    {/* <button onClick={this.props.handleLogout}>Log Out</button> */}
-                    <Button type="submit" onClick={auth.signOut()}>Logout</Button>
-                    {/* <button>Log Out</button> */}
-                </div>
-            ) : ( 
-                <div>
-                    <Redirect to='/'/>
-                </div>
-            )}
-        </div>
-    )
-};
+export default function Dashboard() {
+  const [error, setError] = useState("");
+  const { user, logout } = useAuth();
+  const history = useHistory();
 
-export default Dashboard;
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>Display Name:</strong> {user.displayName}
+          </p>
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
+}
+
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//       {user ? (
+//         <div>
+//           <Navbar />
+//           <p>{`Welcome, ${user.email}`}</p>
+//           <h1>Habits Stuff</h1>
+//           {/* <button onClick={this.props.handleLogout}>Log Out</button> */}
+//           <Button type="submit" onClick={auth.signOut()}>
+//             Logout
+//           </Button>
+//           {/* <button>Log Out</button> */}
+//         </div>
+//       ) : (
+//         <div>
+//           {/* <Redirect to="/" /> */}
+//           <p>{`not logged in`}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
