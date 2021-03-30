2505c807e0e5ce95a7939b43ee17ecc37f90c72c
lohs <55119191+lohs@users.noreply.github.com>
2021-03-30 16:03:07 -0400

    created separate components for login and signup

--- habit-tracker/src/components/Login.js
+++ habit-tracker/src/components/Login.js
@@ -4,35 +4,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useAuth } from "../context/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
-export default function SignUp() {
+export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
+  const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  // async b/c await signup
+  // async b/c await login
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-
-    // try catch block b/c signup is an async fucntion
+    // try catch block b/c login is an async fucntion
     try {
       // reset error
       setError("");
       // loading disables submit button so user cannot spam sign up
       setLoading(true);
-      // wait for signup to finish
-      await signup(emailRef.current.value, passwordRef.current.value);
-      // redirect
+      // wait for login to finish
+      await login(emailRef.current.value, passwordRef.current.value);
       history.push("/");
     } catch {
-      setError("Failed to create an account");
+      setError("Failed to sign in");
     }
 
     // done with try catch
@@ -43,7 +37,7 @@ export default function SignUp() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -54,19 +48,18 @@ export default function SignUp() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
             {/* disable sign up button when trying to submit */}
             <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
+              Log In
             </Button>
           </Form>
+          {/* <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div> */}
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   );
