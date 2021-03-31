e90b7957c43a076026c2827684292ed8914d4da9
lohs <55119191+lohs@users.noreply.github.com>
2021-03-30 22:41:13 -0400

    added add habit modal to dashboard

--- habit-tracker/src/Pages/Dashboard.js
+++ habit-tracker/src/Pages/Dashboard.js
@@ -4,13 +4,18 @@ import Navbar from "../components/Navbar/Navbar";
 // import { auth } from "../firebase";
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.js";
-import { Card, Button, Alert } from "react-bootstrap";
+import { Card, Button, Alert, Modal } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
+import Habits from "../containers/Habits";
+import AddHabit from "../components/AddHabit";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
   const { user, logout } = useAuth();
   const history = useHistory();
+  const [showAddHabit, setShowAddHabit] = useState(false);
+  const handleClose = () => setShowAddHabit(false);
+  const handleShow = () => setShowAddHabit(true);
 
   async function handleLogout() {
     setError("");
@@ -25,26 +30,48 @@ export default function Dashboard() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Display Name:</strong> {user.displayName}
-          </p>
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>Display Name:</strong> {user.displayName}
+            </p>
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
+            </Link>
+            <Button
+              variant="link"
+              onClick={handleLogout}
+              className="w-100 text-center mt-2"
+            >
+              Log Out
+            </Button>
+          </Card.Body>
+        </Card>
       </div>
+
+      {/* add habit modal */}
+      <Button
+        variant="primary"
+        onClick={handleShow}
+        className="btn btn-primary w-100 mt-3"
+      >
+        Add Habit
+      </Button>
+      <Modal show={showAddHabit} onHide={handleClose}>
+        <Modal.Header closeButton />
+        <AddHabit />
+        <Button variant="dark" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal>
+
+      <Habits />
     </>
   );
 }
