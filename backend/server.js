ea1f4223400b3581463a3cfb693012e1e61981e5
lohs <55119191+lohs@users.noreply.github.com>
2021-05-11 14:26:59 -0400

    REVERT BACKEND TO MONGODB ONLY

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,32 @@
-const port = process.env.PORT || 5000;
 const express = require("express");
-const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
 const app = express();
-const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
-io.on("connection", (socket) => {
-  socket.emit("self", socket.id);
+const habitsRouter = require("./routes/habits");
+const usersRouter = require("./routes/users");
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
+app.use("/habits", habitsRouter);
+app.use("/users", usersRouter);
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
-  });
-
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
